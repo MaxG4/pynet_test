15da8f2490092cd75c8b765dffbe918117ccb99d
Max <mbutin@gmail.com>
2020-07-21 12:54:34 -0700

    added original IP for verification

--- list_ex2.py
+++ list_ex2.py
@@ -3,11 +3,12 @@ from __future__ import print_function
 
 ip_add = "192.168.13.1"
 
-my_IP_list = ip_add.split(".")
+my_ip_list = ip_add.split(".")
 
-my_IP_list[-1] = 0
+my_ip_list[-1] = 0
 
 print()
+print(*ip_add)
 print("{:<10} {:<10} {:<10} {:<10}".format("Octect1", "Octect2", "Octect3", "Octect4"))
-print("{:<10} {:<10} {:<10} {:<10}".format(*my_IP_list))
+print("{:<10} {:<10} {:<10} {:<10}".format(*my_ip_list))
 print()
